32ce8af8b256d087ff9631cf336129aacb64aad1
aran-swamy <aran.swamy@gmail.com>
2021-06-19 22:00:43 -0500

    Add multiple digit functionality to addition

--- app.js
+++ app.js
@@ -2,20 +2,29 @@
 const btn = document.querySelector('.talk');
 const inputSpeech = document.querySelector('.inputSpeech');
 const aiOutput = document.querySelector('.aiSpeech');
-const numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven" : 7, "eight" : 8, "nine": 9, "zero" : 0};
+const numbers = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0
+};
 //--------------------------------------
 //make sure browser has supported features
-try{
+try {
     window.SpeechRecognition;
     navigator.geolocation;
-}
-catch(e){
-        try {
-            window.webkitSpeechRecognition;
-        }
-        catch(e){
+} catch (e) {
+    try {
+        window.webkitSpeechRecognition;
+    } catch (e) {
         inputSpeech.textContent = 'Browser Unsupported';
-        }
+    }
 }
 //--------------------------------------
 //global constants (speech rec objects)-
@@ -30,7 +39,7 @@ function getWeather() {
         var lat = document.getElementById('lat').innerHTML //get stored data for use
         var lon = document.getElementById('long').innerHTML //get stored data for use
         const key = '1a1d6997558d87f7f7526a9681af9b3a'; //api key as variable for ease of use
-        var api_link = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+key; //OpenWeatherMap api link
+        var api_link = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key; //OpenWeatherMap api link
         fetch(api_link).then(response => response.json()) //fetch data from api link
             .then(data => {
                 console.log(data);
@@ -41,21 +50,25 @@ function getWeather() {
                 document.getElementById('weather').innerHTML = data['weather'][0]['main'];
             })
     }
+
     navigator.geolocation.getCurrentPosition(success); //actual call to get position via Geolocator api
 }
+
 getWeather(); //used so that weather is updated each time user asks for it, called now to get inital weather data
 //--------------------------------------
 //mic control --------------------------
-btn.addEventListener('click', () => {rec.start();}); //start recording on click
-rec.onstart = function() {
+btn.addEventListener('click', () => {
+    rec.start();
+}); //start recording on click
+rec.onstart = function () {
     console.log('recording started'); //console log for when mic is activated
 };
-rec.onspeechend = function(){
+rec.onspeechend = function () {
     console.log('recording ended'); //console log for when mic is turned off
 }
 //--------------------------------------
 //logging what you said as a string ----
-rec.onresult = function(event){
+rec.onresult = function (event) {
     const current = event.resultIndex;
     const transcript = event.results[current][0].transcript; //string of what you said
     inputSpeech.textContent = "\"" + transcript + "\""; //displaying what you said
@@ -63,36 +76,36 @@ rec.onresult = function(event){
 };
 //--------------------------------------
 //response function (the AI) -----------
-function readOutLoud(message){
+function readOutLoud(message) {
     //transcript/string of what you said/asked is passed in
     const speech = new SpeechSynthesisUtterance(); //the text that the AI will say
     speech.text = ""; //starts empty, is checked later to prevent empty responses
     //if user is greeting the AI -----
-    if(message.includes('hello')){
+    if (message.includes('hello')) {
         //array of possible responses
         const greetings = [
-            'who said you could speak to me?', 
-            'leave me alone.', 
+            'who said you could speak to me?',
+            'leave me alone.',
             'die in a hole.'
         ]
-        const greet = greetings[Math.floor(Math.random()*greetings.length)]; //select a random response
+        const greet = greetings[Math.floor(Math.random() * greetings.length)]; //select a random response
         speech.text = greet; //final output is set
     }
     //-------------------------------
     //if the user is asks how the AI is
-    if(message.includes('how are you')){
+    if (message.includes('how are you')) {
         //array of possible responses
         const howWeBe = [
-            'i was doing better until you showed up', 
-            'i\'m a hardcoded speaking Javascript file you moron, what do you think?', 
+            'i was doing better until you showed up',
+            'i\'m a hardcoded speaking Javascript file you moron, what do you think?',
             'seriously, who asks an \"AI\" made by college kids how they\'re doing.'
         ]
-        const how = howWeBe[Math.floor(Math.random()*howWeBe.length)]; //select a random response
+        const how = howWeBe[Math.floor(Math.random() * howWeBe.length)]; //select a random response
         speech.text += how; //final output is set
     }
     //-------------------------------
     //if user asks for weather ------
-    if(message.includes('weather')){
+    if (message.includes('weather')) {
         getWeather(); //updates weather
         //variables for weather data --
         var temp = Math.floor(Number(document.getElementById('temp').innerHTML) - 273.15);
@@ -123,20 +136,28 @@ function readOutLoud(message){
         speech.text += weatherR; //final output is set
     }
     var toSay = "";
-    if(message.includes('+')) {
-        toSay += "Are you stupid? It's ";
+    if (message.includes('+')) {
+        const startMessage = [
+            'Are you stupid? It\'s ',
+            'Why is this a question that you need to ask me? It\'s ',
+            'You are literally using a webpage, you can just Google it. It\'s ',
+            'And I thought they said humans were the most intelligent creatures on Earth. It\'s ',
+            'Did your math teacher not teach you how to carry? It\'s'
+        ]
+        toSay += startMessage[Math.floor(Math.random() * startMessage.length)];
         const index = message.indexOf("+");
         var value = 0;
-        const first = message.charAt(index -2)
-        const second = message.charAt(index + 2);
-        value = parseInt(first, 10) + parseInt(second,10);
+        const firstIndex = index - 2;
+        const secondIndex = index + 2;
+        //value = parseInt(first, 10) + parseInt(second, 10);
 
-        // let number= 0;
-        // for(let i =index - 1; i > 0; i--) {
-        //     if(message.charAt(i).localeCompare(' ') == 0) {
-        //         number = i;
-        //     }
-        // }
+        let number = 0;
+        for (let i = firstIndex; i > 0; i--) {
+            if (message.charAt(i).localeCompare(' ') == 0) {
+                number = i;
+            }
+        }
+        value += parseInt(message.substring(number + 1, firstIndex +1),10) + parseInt(message.substring(secondIndex,message.length),10);
         console.log(value);
         toSay += value.toString();
         speech.text += toSay;
@@ -150,7 +171,7 @@ function readOutLoud(message){
     speech.volume = 1;
     speech.rate = .95;
     speech.pitch = 1;
-    speech.lang = 1; 
+    speech.lang = 1;
     if (speech.text == "") { //prevent empty response with default/fallback response
         speech.text += "At least tell me something I can understand."
     }
@@ -158,4 +179,5 @@ function readOutLoud(message){
     window.speechSynthesis.speak(speech); //makes AI actually speak + respond
     //-------------------------------
 }
+
 //--------------------------------------
